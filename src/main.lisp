a56f2eaa10b4bad20f27fd76d4c15294d2977928
Eitaro Fukamachi <e.arrows@gmail.com>
2019-03-26 03:11:48 +0900

    Delete spaces at the end of line and add a necessary space between sexp.

--- src/main.lisp
+++ src/main.lisp
@@ -5,7 +5,7 @@
   (:export #:get-place))
 (in-package #:yubin)
 
-(defun get-place (zipcode)      
+(defun get-place (zipcode)
   (let* ((url (format nil "http://zipcloud.ibsnet.co.jp/api/search?zipcode=~A" zipcode))
          (data (reverse (car (fourth (jonathan:parse (dex:get url)))))))
-    (concatenate 'string (first data)(third data) (fifth data))))
+    (concatenate 'string (first data) (third data) (fifth data))))
